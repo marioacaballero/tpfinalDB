88b2b6e43bfe1677ad389c132f1d5bceab88447d
Mario <mario.caballero.948@gmail.com>
2026-06-08 17:53:32 -0300

    schema v1 finalizado

---
+++ sql/01_ddl_schema.sql
@@ -0,0 +1,77 @@
+START TRANSACTION;
+
+CREATE TABLE autor (
+	id_autor INT PRIMARY KEY AUTO_INCREMENT,
+	nombre VARCHAR(50) NOT NULL,
+	apellido VARCHAR(50) NOT NULL,
+	nacionalidad VARCHAR(50)
+);
+
+CREATE TABLE genero (
+	id_genero INT PRIMARY KEY AUTO_INCREMENT,
+	nombre VARCHAR(50) NOT NULL,
+	descripcion VARCHAR(200)
+);
+
+CREATE TABLE libro (
+	isbn VARCHAR(13) PRIMARY KEY,
+	anio_publicacion INT NOT NULL,
+	stock_total INT DEFAULT 0,
+	stock_disponible INT DEFAULT 0 CHECK (stock_disponible >= 0)	
+);
+
+CREATE TABLE libro_genero (
+	id_genero INT NOT NULL,
+	id_libro VARCHAR(13) NOT NULL,
+	PRIMARY KEY (id_genero, id_libro),
+	FOREIGN KEY (id_genero) REFERENCES genero (id_genero),
+	FOREIGN KEY (id_libro) REFERENCES libro (isbn)
+);
+
+CREATE TABLE libro_autor (
+	id_autor INT NOT NULL,
+	id_libro VARCHAR(13) NOT NULL,
+	PRIMARY KEY (id_autor, id_libro),
+	FOREIGN KEY (id_autor) REFERENCES autor (id_autor),
+	FOREIGN KEY (id_libro) REFERENCES libro (isbn)
+);
+
+CREATE TABLE socio (
+	id_socio INT PRIMARY KEY AUTO_INCREMENT,
+	dni VARCHAR(8) NOT NULL UNIQUE,
+	nombre VARCHAR(50) NOT NULL,
+	apellido VARCHAR(50) NOT NULL,
+	email VARCHAR(100) NOT NULL UNIQUE CHECK (email LIKE '%@%'),
+	fecha_alta DATE DEFAULT (CURDATE()),
+	estado VARCHAR(50) DEFAULT 'ACTIVO' CHECK (estado IN ('ACTIVO', 'SUSPENDIDO', 'BAJA'))
+);
+
+CREATE TABLE prestamo (
+	id_prestamo INT PRIMARY KEY AUTO_INCREMENT,
+	fecha_prestamo DATETIME DEFAULT (CURDATE()),
+	fecha_vencimiento DATETIME NOT NULL,
+	fecha_devolucion DATETIME,
+	estado VARCHAR(50) DEFAULT 'ACTIVO' CHECK (estado IN ('ACTIVO', 'DEVUELTO', 'VENCIDO')),
+	id_socio INT NOT NULL,
+	FOREIGN KEY (id_socio) REFERENCES socio (id_socio) ON DELETE CASCADE
+);
+
+CREATE TABLE ejemplar (
+	id_ejemplar INT PRIMARY KEY AUTO_INCREMENT,
+	isbn VARCHAR(13) NOT NULL UNIQUE,
+	nro_ejemplar INT NOT NULL,
+	estado_fisico VARCHAR(50) CHECK (estado_fisico IN ('BUENO', 'REGULAR', 'BAJA')),
+	FOREIGN KEY (isbn) REFERENCES libro (isbn)
+);
+
+CREATE TABLE sancion (
+	id_sancion INT PRIMARY KEY AUTO_INCREMENT,
+	id_socio INT NOT NULL,
+	tipo INT NOT NULL,
+	fecha_inicio DATE NOT NULL,
+	fecha_fin DATE NOT NULL,
+	motivo VARCHAR(100),
+	FOREIGN KEY (id_socio) REFERENCES socio (id_socio)
+);
+
+COMMIT;
